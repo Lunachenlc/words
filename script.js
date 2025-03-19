a79762682f52b8145fe6ff90e858e4f8cd516096
Yanna Chen <chen1002@algonquinlive.com>
2025-03-19 14:31:32 -0400

    11

--- script.js
+++ script.js
@@ -15,3 +15,57 @@ const words = [
 const $form = document.getElementById('form')
 const $word = document.getElementById('word')
 const $words = document.getElementById('words')
+
+/**
+ * displaywords
+ * create a word element for each word in the provided array
+ * @param {array} arods
+ */
+function displayWords (words) {
+  $words.innerHTML = ' '
+  // const $els = words.map(word =>
+  //   createElement('div', { className: 'word' }, word)  不知道做什么的
+  // )
+  $words.append(
+    ...words.map(word => 
+    createElement('div', { className: 'word' }, word)
+  )
+
+  )
+}
+
+displayWords(words)
+
+$words.addEventListener('click', function (e) {
+  if (e.target.classList.contains('word')) {
+    words.splice(words.indexOf(e.target.textContent), 1)
+    displayWords(words)
+  }
+})
+
+const $anchor = document.getElementById('anchor')
+$anchor.addEventListener('click', function (e) {
+  e.preventDefault();
+}
+)
+
+$form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // console.log($word.value)
+  if ($form.checkVisibility())
+  words.push($word.value)
+  displayWords(words)
+  $form.reset()
+}
+)
+
+$word.addEventListener('input', function (e){
+  if (words.includes($word.value)) {
+    // input invalid
+    $word.setCustomValidty('This word is already in the list')
+  } else {
+    // input valid
+    $word.setCustomValidty('')
+  }
+}
+)
